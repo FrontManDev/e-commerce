d53b4d3c26283b7bd034bf7519eed9ef5e15c802
Hamida AbdelKarim <hamidakaim67@example.com>
2025-06-29 12:59:45 -0700

    build category admin page

--- front-end/src/Admin/pages/category/Category.jsx
+++ front-end/src/Admin/pages/category/Category.jsx
@@ -1,9 +1,104 @@
+import './Category.css';
+import { IoAdd } from "react-icons/io5";
+import { useState, useEffect } from 'react';
 export default function Category() {
+
+    const category = [
+        {
+            Id: 1,
+            name: 'phone',
+            Items: 30,
+            Descriptions: 'in category you will finde the best products'
+        },
+        {
+            Id: 2,
+            name: 'Pc',
+            Items: 30,
+            Descriptions: 'in category you will finde the best products'
+        },
+        {
+            Id: 3,
+            name: 'Mac',
+            Items: 30,
+            Descriptions: 'in category you will finde the best products'
+        },
+        {
+            Id: 4,
+            name: 'Samsung',
+            Items: 30,
+            Descriptions: 'in category you will finde the best products'
+        },
+        {
+            Id: 5,
+            name: 'Smart Watch',
+            Items: 30,
+            Descriptions: 'in category you will finde the best products'
+        },
+        {
+            Id: 6,
+            name: 'ear pod',
+            Items: 30,
+            Descriptions: 'in category you will finde the best products'
+        },
+    ];
+
+    const [Category, setCategory] = useState([]);
+    const [firstIndex, setFirstIndex] = useState(0);
+    const [lastIndex, setLastIndex] = useState(5);
+    const [numberPage, setNumberPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    useEffect(() => {
+        setCategory(category);
+        setNumberPage(Math.ceil(category.length / 5));
+    }, []);
     return (
-        <div>
-            <h1>
-                Category
-            </h1>
+        <div className="category-contiaer">
+            <div className="category-heading">
+                <h1>Category</h1>
+                <button><IoAdd />New Category</button>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Items</th>
+                        <th>Desciptions</th>
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Category.slice(firstIndex, lastIndex).map((category) => (
+                            <tr>
+                                <td>{category.Id}</td>
+                                <td>{category.name}</td>
+                                <td>{category.Items}</td>
+                                <td>{category.Descriptions}</td>
+                                <td>
+                                    <button>
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            <div className='button-pages'>
+                {
+                    Array.from({ length: numberPage }).map((_, index) => (
+                        <button onClick={() => {
+                            setFirstIndex(index * 5);
+                            setLastIndex((index + 1) * 5);
+                            setCurrentPage(index);
+                        }} className={index === currentPage ? "active" : ""}>
+                            {index + 1}
+                        </button>
+                    ))
+                }
+            </div>
         </div>
     )
 }
--- front-end/src/Admin/pages/poducts/Products.jsx
+++ front-end/src/Admin/pages/poducts/Products.jsx
@@ -1,7 +1,157 @@
-export default function Products(){
-    return(
-        <div>
-            <h1>Products</h1>
+import { useEffect, useState } from 'react';
+import './Products.css';
+import { IoAdd } from "react-icons/io5";
+
+export default function Products() {
+    const allProducts = [
+        {
+            id: 1,
+            name: "iPhone 14 Pro",
+            category: "Phone",
+            price: 1099,
+            image: "./background-img/ipad.jpg",
+            description: 50
+        },
+        {
+            id: 2,
+            name: "Samsung Galaxy S22",
+            category: "Phone",
+            price: 899,
+            image: "./background-img/ipad.jpg",
+            description: 50
+        },
+        {
+            id: 3,
+            name: "Dell XPS 13",
+            category: "Laptop",
+            price: 1199,
+            image: "./background-img/ipad.jpg",
+            description: 30
+        },
+        {
+            id: 4,
+            name: "MacBook Pro 16",
+            category: "MacBooks",
+            price: 2499,
+            image: "./background-img/ipad.jpg",
+            description: 50
+        },
+        {
+            id: 5,
+            name: "Apple Watch Series 8",
+            category: "Smart Watch",
+            price: 399,
+            image: "./background-img/ipad.jpg",
+            description: 600
+        },
+        {
+            id: 6,
+            name: "Google Pixel 7",
+            category: "Phone",
+            price: 699,
+            image: "./background-img/ipad.jpg",
+            description: 500
+        },
+        {
+            id: 7,
+            name: "Lenovo Legion 5",
+            category: "Laptop",
+            price: 1099,
+            image: "./background-img/ipad.jpg",
+            description: 60
+        },
+        {
+            id: 8,
+            name: "MacBook Air M2",
+            category: "MacBooks",
+            price: 999,
+            image: "./background-img/ipad.jpg",
+            description: 30
+        },
+        {
+            id: 9,
+            name: "Samsung Galaxy Watch 5",
+            category: "Smart Watch",
+            price: 299,
+            image: "./background-img/ipad.jpg",
+            description: 100
+        },
+        {
+            id: 10,
+            name: "Asus ROG Phone 6",
+            category: "Phone",
+            price: 999,
+            image: "./background-img/ipad.jpg",
+            description: 50
+        },
+    ];
+
+    const [products, setProducts] = useState([]);
+    const [firstIndex, setFirstIndex] = useState(0);
+    const [lastIndex, setLastIndex] = useState(5);
+    const [numberPage, setNumberPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    useEffect(() => {
+        setProducts(allProducts);
+        setNumberPage(Math.ceil(allProducts.length / 5));
+    }, []);
+
+    return (
+        <div className='products-container'>
+            <div className="products-heading">
+                <h1>Products</h1>
+                <button><IoAdd />Add New</button>
+            </div>
+            <form>
+                <select>
+                    <option value="">Phone</option>
+                    <option value="">Smart Watch</option>
+                    <option value="">MacBooks</option>
+                    <option value="">Laptop</option>
+                </select>
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.slice(firstIndex, lastIndex).map((product) => (
+                            <tr key={product.id}>
+                                <td>{product.id}</td>
+                                <td><img src={product.image} alt={product.name} width="50" /></td>
+                                <td>{product.name}</td>
+                                <td>{product.category}</td>
+                                <td>${product.price}</td>
+                                <td>{product.description}</td>
+                                <td><button>update</button><button>remove</button></td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            <div className='button-pages'>
+                {
+                    Array.from({length:numberPage}).map((_,index)=>(
+                        <button onClick={()=>{
+                            setFirstIndex(index*5);
+                            setLastIndex((index+1)*5);
+                            setCurrentPage(index);
+                        }} className={index===currentPage ? "active" : ""}>
+                            {index+1}
+                        </button>
+                    ))
+                }
+            </div>
         </div>
-    )
+    );
 }
